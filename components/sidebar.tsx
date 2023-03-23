fb222b63557011c9b24afb218682e717b8169977
MΞCPERSPICACE <71231848+mecperspicace@users.noreply.github.com>
2023-03-23 11:12:55 +0100

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -48,7 +48,7 @@ export default function Navbar() {
           className="flex overflow-hidden flex-row md:flex-col items-start relative px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row md:flex-col space-x-0 pr-10 mb-2 md:mt-0">
+          <div className="flex flex-row md:flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0">
             {navItems[pathname] ? (
               <>
                 {/* Desktop version, hidden on mobile, animates y axis */}
