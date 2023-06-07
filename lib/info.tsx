b9dcc30390d5211f2415c7b238332a1913d19905
MΞCPERSPICACE <71231848+mecperspicace@users.noreply.github.com>
2023-06-07 10:44:18 +0000

    Change the bio in the `info.tsx`

--- lib/info.tsx
+++ lib/info.tsx
@@ -14,7 +14,8 @@ export const bio = () => {
   return (
     <>
       I have always enjoyed learning about new technologies and electronics for
-      a long time and more now. Come find out...
+      a long time and more now. I have chosen mainly scientific subjects at school 
+      and I plan to work in this field later on. Take a look at my projects...
     </>
   );
 };
